35c282e93fab03f9851b9b574f5db9b8b7d7ccab
samuelmenard21 <samuelmenard@gmail.com>
2026-04-30 11:16:42 -0400

    Update DailyGoalRing.jsx

--- shot-club/src/components/DailyGoalRing.jsx
+++ shot-club/src/components/DailyGoalRing.jsx
@@ -1,4 +1,4 @@
-mport { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getTodayProgress } from '../lib/progress'
 
 export default function DailyGoalRing({ playerId, dailyGoal, refreshKey }) {
